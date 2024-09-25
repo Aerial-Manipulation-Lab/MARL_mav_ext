96ebb35f16334627107209e535c4efc884a47fc4
Jack Zeng <jack-zeng@hotmail.com>
2024-09-25 11:54:41 +0200

    skrl files and config

--- exts/MARL_mav_carry_ext/MARL_mav_carry_ext/tasks/MARL_mav_carry/hover/config/flycrane/__init__.py
+++ exts/MARL_mav_carry_ext/MARL_mav_carry_ext/tasks/MARL_mav_carry/hover/config/flycrane/__init__.py
@@ -7,9 +7,10 @@ from . import agents
 gym.register(
     id="Isaac-flycrane-payload-hovering-v0",
     entry_point="omni.isaac.lab.envs:ManagerBasedRLEnv",
-    disable_env_checker=False,
+    disable_env_checker=True,
     kwargs={
         "env_cfg_entry_point": HoverEnvCfg,
         "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:FlycraneHoverPPORunnerCfg",
+        "skrl_cfg_entry_point": f"{agents.__name__}.skrl_ppo_cfg.yaml",
     },
 )
