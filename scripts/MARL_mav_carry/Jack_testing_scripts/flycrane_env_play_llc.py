d85e25883843affbc1ee8415fef95135dba074e5
Jack Zeng <jack-zeng@hotmail.com>
2024-10-08 15:42:27 +0200

    dummy values to test pipeline

--- scripts/MARL_mav_carry/Jack_testing_scripts/flycrane_env_play_llc.py
+++ scripts/MARL_mav_carry/Jack_testing_scripts/flycrane_env_play_llc.py
@@ -57,73 +57,73 @@ def main():
     # print(env.scene["robot"].root_physx_view.get_masses())
     # simulate physics
     # Example usage: define 3D waypoints and corresponding timestamps
-    waypoints_3d = torch.tensor([0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0,# point 1
-                                 5, 5, 5, 1, -1, 1, 1, -5, 1, 0.0007963, 0, 0, 1, # point 2
-                                 10, 0, 10, 1, -1, -1, 1, -1, -1, 0.4999998, -0.4996018, 0.4999998, 0.5003982, # point 3
-                                 15, -5, 5, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0], dtype=torch.float32) # point 4 # Positions (x, y, z) at each time point
-    times = torch.tensor([0, 2, 4, 6], dtype=torch.float32)  # Timestamps
+    # waypoints_3d = torch.tensor([0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0,# point 1
+    #                              5, 5, 5, 1, -1, 1, 1, -5, 1, 0.0007963, 0, 0, 1, # point 2
+    #                              10, 0, 10, 1, -1, -1, 1, -1, -1, 0.4999998, -0.4996018, 0.4999998, 0.5003982, # point 3
+    #                              15, -5, 5, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0], dtype=torch.float32) # point 4 # Positions (x, y, z) at each time point
+    # times = torch.tensor([0, 2, 4, 6], dtype=torch.float32)  # Timestamps
 
-    # Generate the minimum snap spline
-    coeffs_list_3d, orientations_traj = minimum_snap_spline(waypoints_3d, times)
+    # # Generate the minimum snap spline
+    # coeffs_list_3d, orientations_traj = minimum_snap_spline(waypoints_3d, times)
 
-    # Evaluate the trajectory at different time points
-    positions = []
-    velocities = []
-    accelerations = []
-    orientations = []
-    angular_rates = []
-    eval_times = torch.linspace(0, 6, 100)
-    for t_eval in eval_times:
-        position, velocity, acceleration, jerk, snap, orientation, angular_velocity = evaluate_trajectory(coeffs_list_3d, orientations_traj, times, t_eval)
-        positions.append(position)
-        velocities.append(velocity)
-        accelerations.append(acceleration)
-        orientations.append(orientation)
-        angular_rates.append(angular_velocity)
+    # # Evaluate the trajectory at different time points
+    # positions = []
+    # velocities = []
+    # accelerations = []
+    # orientations = []
+    # angular_rates = []
+    # eval_times = torch.linspace(0, 6, 100)
+    # for t_eval in eval_times:
+    #     position, velocity, acceleration, jerk, snap, orientation, angular_velocity = evaluate_trajectory(coeffs_list_3d, orientations_traj, times, t_eval)
+    #     positions.append(position)
+    #     velocities.append(velocity)
+    #     accelerations.append(acceleration)
+    #     orientations.append(orientation)
+    #     angular_rates.append(angular_velocity)
 
-    positions = torch.stack(positions, dim=0)
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.plot(positions[:, 0], positions[:, 1], positions[:, 2])
-    ax.set_xlabel('X')
-    ax.set_ylabel('Y')
-    ax.set_zlabel('Z')
+    # positions = torch.stack(positions, dim=0)
+    # fig = plt.figure()
+    # ax = fig.add_subplot(111, projection='3d')
+    # ax.plot(positions[:, 0], positions[:, 1], positions[:, 2])
+    # ax.set_xlabel('X')
+    # ax.set_ylabel('Y')
+    # ax.set_zlabel('Z')
 
-    # Plot velocities against time
-    plt.figure()
-    plt.plot(eval_times, velocities)
-    plt.xlabel('Time')
-    plt.ylabel('Velocities')
-    plt.title('Velocities vs Time')
-    plt.legend(['X', 'Y', 'Z'])
+    # # Plot velocities against time
+    # plt.figure()
+    # plt.plot(eval_times, velocities)
+    # plt.xlabel('Time')
+    # plt.ylabel('Velocities')
+    # plt.title('Velocities vs Time')
+    # plt.legend(['X', 'Y', 'Z'])
 
-    # Plot accelerations against time
-    plt.figure()
-    plt.plot(eval_times, accelerations)
-    plt.xlabel('Time')
-    plt.ylabel('Accelerations')
-    plt.title('Accelerations vs Time')
-    plt.legend(['X', 'Y', 'Z'])
+    # # Plot accelerations against time
+    # plt.figure()
+    # plt.plot(eval_times, accelerations)
+    # plt.xlabel('Time')
+    # plt.ylabel('Accelerations')
+    # plt.title('Accelerations vs Time')
+    # plt.legend(['X', 'Y', 'Z'])
 
-    # plot orientation against time
-    orientations = torch.stack(orientations, dim=0)
-    plt.figure()
-    plt.plot(eval_times, orientations)
-    plt.xlabel('Time')
-    plt.ylabel('Orientation')
-    plt.title('Orientation vs Time')
-    plt.legend(['W','X', 'Y', 'Z'])
+    # # plot orientation against time
+    # orientations = torch.stack(orientations, dim=0)
+    # plt.figure()
+    # plt.plot(eval_times, orientations)
+    # plt.xlabel('Time')
+    # plt.ylabel('Orientation')
+    # plt.title('Orientation vs Time')
+    # plt.legend(['W','X', 'Y', 'Z'])
 
-    # plot angular rates against time
-    angular_rates = torch.stack(angular_rates, dim=0)
-    plt.figure()
-    plt.plot(eval_times, angular_rates) 
-    plt.xlabel('Time')
-    plt.ylabel('Angular Rates')
-    plt.title('Angular Rates vs Time')
-    plt.legend(['X', 'Y', 'Z'])
+    # # plot angular rates against time
+    # angular_rates = torch.stack(angular_rates, dim=0)
+    # plt.figure()
+    # plt.plot(eval_times, angular_rates) 
+    # plt.xlabel('Time')
+    # plt.ylabel('Angular Rates')
+    # plt.title('Angular Rates vs Time')
+    # plt.legend(['X', 'Y', 'Z'])
 
-    plt.show()
+    # plt.show()
 
     count = 0
     while simulation_app.is_running():
@@ -136,14 +136,20 @@ def main():
                 print("[INFO]: Resetting environment...")
                 # print("obs manager", env.observation_manager.compute())
             # sample random actions
-            waypoint = torch.zeros_like(env.action_manager.action)
-            waypoint[:, 0] = mass_left_side * gravity / 2
-            waypoint[:, 4] = mass_left_side * gravity / 2
-            waypoint[:, 8] = mass_right_side * gravity
-            # waypoint[:, 6] = 0.05
-
+            waypoints_3d = torch.tensor([[0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0,# point 1
+                                 5, 5, 5, 1, -1, 1, 1, -5, 1, 0.0007963, 0, 0, 1, # point 2
+                                 10, 0, 10, 1, -1, -1, 1, -1, -1, 0.4999998, -0.4996018, 0.4999998, 0.5003982, # point 3
+                                 15, -5, 5, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0,
+                                 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0,# point 1
+                                 5, 5, 5, 1, -1, 1, 1, -5, 1, 0.0007963, 0, 0, 1, # point 2
+                                 10, 0, 10, 1, -1, -1, 1, -1, -1, 0.4999998, -0.4996018, 0.4999998, 0.5003982, # point 3
+                                 15, -5, 5, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0,
+                                 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0,# point 1
+                                 5, 5, 5, 1, -1, 1, 1, -5, 1, 0.0007963, 0, 0, 1, # point 2
+                                 10, 0, 10, 1, -1, -1, 1, -1, -1, 0.4999998, -0.4996018, 0.4999998, 0.5003982, # point 3
+                                 15, -5, 5, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0]], dtype=torch.float32) # point 4 # Positions (x, y, z) at each time point
             # step the environment
-            obs, rew, terminated, truncated, info = env.step(waypoint * 1)
+            obs, rew, terminated, truncated, info = env.step(waypoints_3d)
             # print current orientation of pole
             # print("[Env 0]: Pole joint: ", obs["policy"][0][1].item())
             # update counter
