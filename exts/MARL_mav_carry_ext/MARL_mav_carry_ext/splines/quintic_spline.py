449ab2f8d4948f39ce40a66f509d2ddfd7cacba3
Jack Zeng <jack-zeng@hotmail.com>
2024-10-11 17:00:02 +0200

    put tensors on device

--- exts/MARL_mav_carry_ext/MARL_mav_carry_ext/splines/quintic_spline.py
+++ exts/MARL_mav_carry_ext/MARL_mav_carry_ext/splines/quintic_spline.py
@@ -11,12 +11,12 @@ def quintic_trajectory(start, end, t_start, t_end):
         [5*T**4, 4*T**3, 3*T**2, 2*T, 1, 0],  # v(t1)
         [0, 0, 0, 2, 0, 0],             # a(t0)
         [20*T**3, 12*T**2, 6*T, 2, 0, 0]     # a(t1)
-    ], dtype=torch.float32)
+    ], dtype=torch.float32, device=torch.device('cuda'))
     
     # Position polynomials
-    b_x = torch.tensor([start['pos'][0], end['pos'][0], start['vel'][0], end['vel'][0], start['acc'][0], end['acc'][0]], dtype=torch.float32)
-    b_y = torch.tensor([start['pos'][1], end['pos'][1], start['vel'][1], end['vel'][1], start['acc'][1], end['acc'][1]], dtype=torch.float32)
-    b_z = torch.tensor([start['pos'][2], end['pos'][2], start['vel'][2], end['vel'][2], start['acc'][2], end['acc'][2]], dtype=torch.float32)
+    b_x = torch.tensor([start['pos'][0], end['pos'][0], start['vel'][0], end['vel'][0], start['acc'][0], end['acc'][0]], dtype=torch.float32, device=torch.device('cuda'))
+    b_y = torch.tensor([start['pos'][1], end['pos'][1], start['vel'][1], end['vel'][1], start['acc'][1], end['acc'][1]], dtype=torch.float32, device=torch.device('cuda'))
+    b_z = torch.tensor([start['pos'][2], end['pos'][2], start['vel'][2], end['vel'][2], start['acc'][2], end['acc'][2]], dtype=torch.float32, device=torch.device('cuda'))
     
     coeffs_x = torch.linalg.solve(A, b_x)
     coeffs_y = torch.linalg.solve(A, b_y)
@@ -27,13 +27,13 @@ def quintic_trajectory(start, end, t_start, t_end):
 # Compute derivatives of the polynomial (velocity, acceleration, jerk, snap)
 def compute_derivatives(coeffs, power):
     if power == 1:
-        return coeffs[:-1] * torch.tensor([5, 4, 3, 2, 1], dtype=torch.float32)
+        return coeffs[:-1] * torch.tensor([5, 4, 3, 2, 1], dtype=torch.float32, device=torch.device('cuda'))
     elif power == 2:
-        return coeffs[:-2] * torch.tensor([20, 12, 6, 2], dtype=torch.float32)
+        return coeffs[:-2] * torch.tensor([20, 12, 6, 2], dtype=torch.float32, device=torch.device('cuda'))
     elif power == 3:
-        return coeffs[:-3] * torch.tensor([60, 24, 6], dtype=torch.float32)
+        return coeffs[:-3] * torch.tensor([60, 24, 6], dtype=torch.float32, device=torch.device('cuda'))
     elif power == 4:
-        return coeffs[:-4] * torch.tensor([120, 24], dtype=torch.float32)
+        return coeffs[:-4] * torch.tensor([120, 24], dtype=torch.float32, device=torch.device('cuda'))
     else:
         return coeffs
 
@@ -71,36 +71,35 @@ def evaluate_trajectory(coeffs_list, times, t_eval):
             coeffs_x, coeffs_y, coeffs_z = coeffs_list[i]
             
             # Compute position (5th order polynomial)
-            position_x = torch.sum(coeffs_x * torch.tensor([T**5, T**4, T**3, T**2, T, 1], dtype=torch.float32))
-            position_y = torch.sum(coeffs_y * torch.tensor([T**5, T**4, T**3, T**2, T, 1], dtype=torch.float32))
-            position_z = torch.sum(coeffs_z * torch.tensor([T**5, T**4, T**3, T**2, T, 1], dtype=torch.float32))
+            position_x = torch.sum(coeffs_x * torch.tensor([T**5, T**4, T**3, T**2, T, 1], dtype=torch.float32, device=torch.device('cuda')))
+            position_y = torch.sum(coeffs_y * torch.tensor([T**5, T**4, T**3, T**2, T, 1], dtype=torch.float32, device=torch.device('cuda')))
+            position_z = torch.sum(coeffs_z * torch.tensor([T**5, T**4, T**3, T**2, T, 1], dtype=torch.float32, device=torch.device('cuda')))
             position = torch.tensor([position_x, position_y, position_z], dtype=torch.float32, device=torch.device('cuda'))
             
             # Compute velocity
-            velocity_x = torch.sum(compute_derivatives(coeffs_x, 1) * torch.tensor([T**4, T**3, T**2, T, 1], dtype=torch.float32))
-            velocity_y = torch.sum(compute_derivatives(coeffs_y, 1) * torch.tensor([T**4, T**3, T**2, T, 1], dtype=torch.float32))
-            velocity_z = torch.sum(compute_derivatives(coeffs_z, 1) * torch.tensor([T**4, T**3, T**2, T, 1], dtype=torch.float32))
+            velocity_x = torch.sum(compute_derivatives(coeffs_x, 1) * torch.tensor([T**4, T**3, T**2, T, 1], dtype=torch.float32, device=torch.device('cuda')))
+            velocity_y = torch.sum(compute_derivatives(coeffs_y, 1) * torch.tensor([T**4, T**3, T**2, T, 1], dtype=torch.float32, device=torch.device('cuda')))
+            velocity_z = torch.sum(compute_derivatives(coeffs_z, 1) * torch.tensor([T**4, T**3, T**2, T, 1], dtype=torch.float32, device=torch.device('cuda')))
             velocity = torch.tensor([velocity_x, velocity_y, velocity_z], dtype=torch.float32, device=torch.device('cuda'))
             
             # Compute acceleration
-            acceleration_x = torch.sum(compute_derivatives(coeffs_x, 2) * torch.tensor([T**3, T**2, T, 1], dtype=torch.float32))
-            acceleration_y = torch.sum(compute_derivatives(coeffs_y, 2) * torch.tensor([T**3, T**2, T, 1], dtype=torch.float32))
-            acceleration_z = torch.sum(compute_derivatives(coeffs_z, 2) * torch.tensor([T**3, T**2, T, 1], dtype=torch.float32))
+            acceleration_x = torch.sum(compute_derivatives(coeffs_x, 2) * torch.tensor([T**3, T**2, T, 1], dtype=torch.float32, device=torch.device('cuda')))
+            acceleration_y = torch.sum(compute_derivatives(coeffs_y, 2) * torch.tensor([T**3, T**2, T, 1], dtype=torch.float32, device=torch.device('cuda')))
+            acceleration_z = torch.sum(compute_derivatives(coeffs_z, 2) * torch.tensor([T**3, T**2, T, 1], dtype=torch.float32, device=torch.device('cuda')))
             acceleration = torch.tensor([acceleration_x, acceleration_y, acceleration_z], dtype=torch.float32, device=torch.device('cuda'))
             
             # Compute jerk
-            jerk_x = torch.sum(compute_derivatives(coeffs_x, 3) * torch.tensor([T**2, T, 1], dtype=torch.float32))
-            jerk_y = torch.sum(compute_derivatives(coeffs_y, 3) * torch.tensor([T**2, T, 1], dtype=torch.float32))
-            jerk_z = torch.sum(compute_derivatives(coeffs_z, 3) * torch.tensor([T**2, T, 1], dtype=torch.float32))
+            jerk_x = torch.sum(compute_derivatives(coeffs_x, 3) * torch.tensor([T**2, T, 1], dtype=torch.float32, device=torch.device('cuda')))
+            jerk_y = torch.sum(compute_derivatives(coeffs_y, 3) * torch.tensor([T**2, T, 1], dtype=torch.float32, device=torch.device('cuda')))
+            jerk_z = torch.sum(compute_derivatives(coeffs_z, 3) * torch.tensor([T**2, T, 1], dtype=torch.float32, device=torch.device('cuda')))
             jerk = torch.tensor([jerk_x, jerk_y, jerk_z], dtype=torch.float32, device=torch.device('cuda'))
             
             # Compute snap
-            snap_x = torch.sum(compute_derivatives(coeffs_x, 4) * torch.tensor([T, 1], dtype=torch.float32))
-            snap_y = torch.sum(compute_derivatives(coeffs_y, 4) * torch.tensor([T, 1], dtype=torch.float32))
-            snap_z = torch.sum(compute_derivatives(coeffs_z, 4) * torch.tensor([T, 1], dtype=torch.float32))
+            snap_x = torch.sum(compute_derivatives(coeffs_x, 4) * torch.tensor([T, 1], dtype=torch.float32, device=torch.device('cuda')))
+            snap_y = torch.sum(compute_derivatives(coeffs_y, 4) * torch.tensor([T, 1], dtype=torch.float32, device=torch.device('cuda')))
+            snap_z = torch.sum(compute_derivatives(coeffs_z, 4) * torch.tensor([T, 1], dtype=torch.float32, device=torch.device('cuda')))
             snap = torch.tensor([snap_x, snap_y, snap_z], dtype=torch.float32, device=torch.device('cuda'))
             
             return position, velocity, acceleration, jerk, snap
         
-        raise ValueError("Time t_eval is out of range of the trajectory")
-    return None
+    raise ValueError("Time t_eval is out of range of the trajectory")
