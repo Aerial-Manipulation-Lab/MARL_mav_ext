ba54edc1e1d9e3108e04c66b515261e628bd137c
Jack Zeng <jack-zeng@hotmail.com>
2024-11-04 14:18:34 +0100

    video option spline llc

--- scripts/MARL_mav_carry/Jack_testing_scripts/flycrane_env_play_llc_spline.py
+++ scripts/MARL_mav_carry/Jack_testing_scripts/flycrane_env_play_llc_spline.py
@@ -12,17 +12,22 @@ This script demonstrates how to simulate a quadcopter.
 
 import argparse
 import torch
+import os
 
 from omni.isaac.lab.app import AppLauncher
 
 # add argparse arguments
 parser = argparse.ArgumentParser(description="This script demonstrates how to simulate a quadcopter.")
 parser.add_argument("--num_envs", type=int, default=1, help="Number of environments to spawn.")
+parser.add_argument("--video", action="store_true", default=False, help="Record videos during execution.")
+parser.add_argument("--video_length", type=int, default=200, help="Length of the recorded video (in steps).")
 
 # append AppLauncher cli args
 AppLauncher.add_app_launcher_args(parser)
 # parse the arguments
 args_cli = parser.parse_args()
+if args_cli.video:
+    args_cli.enable_cameras = True
 
 # launch omniverse app
 app_launcher = AppLauncher(args_cli)
@@ -32,13 +37,13 @@ simulation_app = app_launcher.app
 
 import math
 import matplotlib.pyplot as plt
-from gymnasium.spaces import Box
+import gymnasium as gym
 
 from MARL_mav_carry_ext.splines import septic_spline
 from MARL_mav_carry_ext.tasks.MARL_mav_carry.hover_llc.hover_env_cfg_spline import HoverEnvCfg_llc_spline
 
 from omni.isaac.lab.envs import ManagerBasedRLEnv
-
+from omni.isaac.lab.utils.dict import print_dict
 
 def main():
     """Main function."""
@@ -46,8 +51,18 @@ def main():
     env_cfg = HoverEnvCfg_llc_spline()
     env_cfg.scene.num_envs = args_cli.num_envs
     # setup RL environment
-    env = ManagerBasedRLEnv(cfg=env_cfg)
-    env.action_space = Box(-1.0, 1.0, shape=(env.scene.num_envs, 12), dtype="float32")
+    env = ManagerBasedRLEnv(cfg=env_cfg, render_mode="rgb_array" if args_cli.video else None)
+    if args_cli.video:
+        video_kwargs = {
+            "video_folder": "./videos",
+            "step_trigger": lambda step: step == 0,
+            "video_length": args_cli.video_length,
+            "disable_logger": True,
+        }
+        print("[INFO] Recording videos during training.")
+        print_dict(video_kwargs, nesting=4)
+        env = gym.wrappers.RecordVideo(env, **video_kwargs)
+
     robot_mass = env.scene["robot"].root_physx_view.get_masses().sum()
     gravity = torch.tensor(env.sim.cfg.gravity, device=env.sim.device).norm()
     falcon_mass = 0.6 + 0.0042 * 4 + 0.00002
@@ -61,7 +76,6 @@ def main():
         with torch.inference_mode():
             # reset
             if count % 500 == 0:
-                count = 0
                 env.reset()
                 print("-" * 80)
                 print("[INFO]: Resetting environment...")
@@ -90,6 +104,9 @@ def main():
             obs, rew, terminated, truncated, info = env.step(waypoint)
             # update counter
             count += 1
+
+            if count == args_cli.video_length:
+                break
 
     # close the simulator
     env.close()
