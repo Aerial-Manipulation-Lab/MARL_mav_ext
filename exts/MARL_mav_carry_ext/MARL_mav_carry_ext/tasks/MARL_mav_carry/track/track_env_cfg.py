5f4de365f08551fb2b4688d9a5f8e6100d940aa7
Jack Zeng <jack-zeng@hotmail.com>
2024-11-29 16:26:43 +0100

    re-add positions, needed for DFBC

--- exts/MARL_mav_carry_ext/MARL_mav_carry_ext/tasks/MARL_mav_carry/track/track_env_cfg.py
+++ exts/MARL_mav_carry_ext/MARL_mav_carry_ext/tasks/MARL_mav_carry/track/track_env_cfg.py
@@ -85,11 +85,13 @@ class ObservationsCfg:
         """Observation terms for the policy."""
 
         # payload and drone states
+        payload_pos = ObsTerm(func=mdp.payload_position)  # can add noise later
         payload_orientation = ObsTerm(func=mdp.payload_orientation)
         payload_linear_velocities = ObsTerm(func=mdp.payload_linear_velocities)
         payload_angular_velocities = ObsTerm(func=mdp.payload_angular_velocities)
         payload_linear_accelerations = ObsTerm(func=mdp.payload_linear_acceleration)
         payload_angular_accelerations = ObsTerm(func=mdp.payload_angular_acceleration)
+        drone_positions = ObsTerm(func=mdp.drone_positions)
         drone_orientations = ObsTerm(func=mdp.drone_orientations)
         drone_linear_velocities = ObsTerm(func=mdp.drone_linear_velocities)
         drone_angular_velocities = ObsTerm(func=mdp.drone_angular_velocities)
