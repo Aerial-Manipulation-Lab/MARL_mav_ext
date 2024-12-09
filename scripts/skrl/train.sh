c7c3dd79b1cf6dad5a7e08c3873b2f215556b495
Jack Zeng <jack-zeng@hotmail.com>
2024-12-09 17:58:20 +0100

    restart from scratch if training fails

--- scripts/skrl/train.sh
+++ scripts/skrl/train.sh
@@ -3,13 +3,10 @@
 # Default values
 TASK="Isaac-flycrane-payload-hovering-llc-v0"
 NUM_ENVS=20480
-RESUME=false
 
 # Parse command-line arguments
 while [[ "$#" -gt 0 ]]; do
     case $1 in
-        --resume) RESUME=true; shift ;;
-        --checkpoint) CHECKPOINT="$2"; shift ;;
         --task) TASK="$2"; shift ;;
         --num_envs) NUM_ENVS="$2"; shift ;;
         *) echo "Unknown parameter passed: $1"; exit 1 ;;
@@ -18,15 +15,11 @@ while [[ "$#" -gt 0 ]]; do
 done
 
 # Run the training script
-if [ "$RESUME" = true ]; then
-    python3 scripts/skrl/train.py --task $TASK --num_envs $NUM_ENVS --resume --checkpoint $CHECKPOINT --headless --seed=-1
-else
-    python3 scripts/skrl/train.py --task $TASK --num_envs $NUM_ENVS --headless --seed=-1
-    echo "Start training from scratch"
-fi
+python3 scripts/skrl/train.py --task $TASK --num_envs $NUM_ENVS --headless --seed=-1
+echo "Start training from scratch"
 
-# if it crashed, keep restarting with resume from the last checkpoint
+# if it crashed, keep restarting
 while true; do
     echo "Restarting training..."
-    python3 scripts/skrl/train.py --task $TASK --num_envs $NUM_ENVS --resume --headless --seed=-1
+    python3 scripts/skrl/train.py --task $TASK --num_envs $NUM_ENVS --headless --seed=-1
 done
