d52a12f5dd721aabfb9f3c82bddf25daa82110aa
Jack Zeng <jack-zeng@hotmail.com>
2025-03-28 10:08:19 +0100

    increase drone distance termination threshold hover

--- exts/MARL_mav_carry_ext/MARL_mav_carry_ext/tasks/directMARL/hover/marl_hover_env_cfg.py
+++ exts/MARL_mav_carry_ext/MARL_mav_carry_ext/tasks/directMARL/hover/marl_hover_env_cfg.py
@@ -151,7 +151,7 @@ class MARLHoverEnvCfg(DirectMARLEnvCfg):
     cable_angle_limits_payload = -math.sqrt(2) / 2  # cos(angle) limits
     cable_collision_threshold = 0.2
     cable_collision_num_points = 10
-    drone_collision_threshold = 0.5
+    drone_collision_threshold = 0.8
     bounding_box_threshold = 5.0
 
     # low level control
